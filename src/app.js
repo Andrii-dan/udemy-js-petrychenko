ecc491851a3194805902440c4e94e3457f31c52c
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2023-01-04 22:44:24 +0100

    refactoring

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const numberOfFilms = +prompt('How many movies did you watch?', '');
-const lastWatchedMovies = prompt('Some many movies that you watch?');
+const lastWatchedMovies = prompt('Some movies that you watch?');
 const movieRating = prompt('How do you evaluete them?');
 
 const personalMoviesDB = {
