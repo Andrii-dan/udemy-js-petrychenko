6e151dc435e49f9d20d51f4208cee4098920d94a
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2023-01-09 07:13:01 +0100

    loop tasks

--- src/app.js
+++ src/app.js
@@ -1,21 +1,56 @@
 'use strict';
 
-const arr = []
-
-for (let i = 0; i < 5; i++) {
-	arr.push('*')
-	console.log(arr.join(""));
-} 
-
-let tree = '';
-
-for ( let i = 1; i < 7; i++) {
-	
-	for (j = 1; j < i; j++) {
-		tree += '*'
-	}
-
-	tree += '\n'
+// 1st task
+for (let i = 5; i <= 10; i++) {
+	console.log(i);
 }
 
-console.log(tree);
+let num = 4;
+
+do {
+	num++;
+	console.log(num);
+} while (num < 10);
+
+//second task
+for (let j = 20; j >= 10; j--) {
+	if (j === 13) {
+		break;
+	}
+	console.log(j);
+}
+
+// third task
+for (let i = 2; i <= 10; i++) {
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+}
+
+//fourth task
+for (let i = 2; i <= 16; i++) {
+	if (i % 2 === 0) {
+		continue;
+	} else {
+		console.log(i);
+	}
+}
+
+let number = 2;
+
+while (number < 16) {
+	number++;
+	if (number % 2 === 0) {
+		continue;
+	} else {
+		console.log(number);
+	}
+}
+
+//fifth task
+let arr = [];
+
+for (let i = 0; i <= 5; i++) {
+	arr[i] = i + 5;
+}
+console.log(arr);
