41656979432a66f7135171444821b484e63a90aa
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2023-01-10 21:06:52 +0100

    the hardest task

--- src/app.js
+++ src/app.js
@@ -32,3 +32,20 @@ for (let i = 0; i < data1.length; i++) {
 }
 
 console.log(data2);
+
+
+//harder task
+const lines = 5;
+let result1 = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result1 += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result1 += "*";
+    }
+    result1 += "\n";
+}
+
+console.log(result1)
