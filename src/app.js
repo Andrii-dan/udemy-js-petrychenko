5f2c9c86c912fe791f58d442efbb0a05e2bf773a
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2023-01-22 22:15:11 +0100

    styling DOM elements in js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,19 @@
 'use strict';
 
-// how to get elements from DOM
-// by id - returns one element with entered id name
 const box = document.getElementById('box');
-console.log(box);
 
-// by by tag name - returns all elements in DOM with entered tag
 const btns = document.getElementsByTagName('button');
-console.log(btns);
 
-// by class - returns all elements that have entered class
 const circles = document.getElementsByClassName('circle');
-console.log(circles);
 
-// by class, id, tag name and other selectros (similar to css selectors) returns nodeList with all elements that match entered value
 const hearts = document.querySelectorAll('.heart');
-console.log(hearts);
 
-// we can call methods on nodeList
-hearts.forEach((el) => {
-	console.log(el);
-});
-
-// returns one (first) element that match entered value
 const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+
+box.style.backgroundColor = 'blue';
+box.style.width = '200px';
+box.style.height = '200px';
+
+box.style.cssText = 'background: blue; width: 500px; height: 500px;';
+
+btns[1].style.borderRadius = '50px';
