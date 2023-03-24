047b44f623fe59ce5779bcc15221fa3090116b93
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2023-03-24 18:58:43 +0100

    Promise, then and catch

--- src/app.js
+++ src/app.js
@@ -1,58 +1,41 @@
 'use strict';
 
-const box = document.getElementById('box');
+console.log('Request pending...');
 
-const btns = document.getElementsByTagName('button');
+const req = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		console.log('Loading...');
 
-const circles = document.getElementsByClassName('circle');
+		const product = {
+			name: 'tv',
+			price: 200,
+		};
 
-const square = document.querySelectorAll('.square');
+		resolve(product);
+		// reject()
+	}, 2000);
+});
 
-const oneSquare = document.querySelector('.square');
-
-box.style.backgroundColor = 'blue';
-box.style.width = '200px';
-box.style.height = '200px';
-
-box.style.cssText = 'background: blue; width: 500px; height: 500px;';
-
-btns[1].style.borderRadius = '50px';
-
-// for (let i = 0; i < square.length; i++) {
-// 	square[i].style.cssText =
-// 		'background: pink; width: 40px; height: 40px; margin: 10px;';
-// }
-
-square.forEach(
-	(item) =>
-		(item.style.cssText =
-			'background: pink; width: 40px; height: 40px; margin: 10px;')
-);
-
-// syntax to create a DOM element in js file
-const div = document.createElement('div');
-
-// to add a class for a DOM element
-div.classList.add('black');
-
-// to insert element in DOM (or inside DOM element) as last child
-document.body.append(div);
-// box.append(div);
-
-// to insert element in DOM (or inside DOM element) as first child
-document.body.prepend(div)
-
-// to insert before specific element
-square[1].before(div)
-
-// to insert after specific element
-square[1].after(div)
-
-// to remove element from DOM
-circles[1].remove()
-
-// to replace one element with another
-square[1].replaceWith(circles[0])
-
-// to insert text inside DOM element
-div.textContent = 'Hello World!'
+req
+	.then((product) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				product.status = 'order';
+				resolve(product);
+			}, 2000);
+		});
+	})
+	.then((data) => {
+		data.modify = true;
+		return data;
+	})
+	.then((data) => {
+		console.log(data);
+	})
+	.catch(() => {
+		console.error('Something went wrong');
+	})
+	.finally(() => {
+        // works regardless of resolve or reject 
+		console.log('Request finished');
+	});
