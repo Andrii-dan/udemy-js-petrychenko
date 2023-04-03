117d7e1ac6683ecd19fdc63b9abdb8a30810771f
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2023-04-03 21:40:26 +0200

    array tasks

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 'use strict';
 
-const factorial = (n) => {
-	if (n >= 1) {
-		return n * factorial(n - 1);
+// 1) You have an array with income data for each outlet. Write a getPositiveIncomeAmount function that takes this array of data and returns the sum of only the positive values ​​from each object.
+
+const funds = [
+	{ amount: -1400 },
+	{ amount: 2400 },
+	{ amount: -1000 },
+	{ amount: 500 },
+	{ amount: 10400 },
+	{ amount: -11400 },
+];
+
+// Example:
+// getPositiveIncomeAmount(funds) => 13300
+
+const getPositiveIncomeAmount = (data) => {
+	const result = data
+		.filter((el) => el.amount > 0)
+		.map((item) => item.amount)
+		.reduce((a, b) => a + b);
+
+	return result;
+};
+
+console.log(getPositiveIncomeAmount(funds));
+
+// 2) Write a getTotalIncomeAmount function that also accepts this data array. If at least one of the objects contains a negative amount field value, then the function returns the sum of all values. (number) If there are no such values, the getPositiveIncomeAmount function is launched with the same data array.
+
+// Example:
+// getTotalIncomeAmount(funds) => -500
+
+const getTotalIncomeAmount = (data) => {
+	const isSomefundNegative = data.some((el) => el.amount < 0);
+
+	if (isSomefundNegative) {
+		return data.map((item) => item.amount).reduce((a, b) => a + b);
 	} else {
-		return 1;
+		return getPositiveIncomeAmount(data);
 	}
 };
 
-console.log(factorial(5));
+console.log(getTotalIncomeAmount(funds));
